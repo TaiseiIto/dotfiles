0e151f466d81e4c2e3d84084c9e56044c9aea286
TaiseiIto <taisei1212@outlook.jp>
2023-10-25 23:42:17 +0900

    add username setting

--- build.bat
+++ build.bat
@@ -4,6 +4,11 @@ set image_name=dotfiles
 set container_name=dotfiles
 set image_exists=false
 set container_exists=false
+for /f "delims=" %%i in ('git config --get remote.origin.url') do (
+	for /f "tokens=2 delims=:/" %%j in ("%%i") do (
+		set user_name=%%j
+	)
+)
 for /f "tokens=*" %%i in ('docker images --format "{{.Repository}}"') do (
 	if %%i==dotfiles (
 		set image_exists=true
@@ -15,7 +20,7 @@ for /f "tokens=*" %%i in ('docker ps -a --format "{{.Names}}"') do (
 	)
 )
 if !image_exists!==false (
-	docker compose build
+	docker compose build --build-arg USERNAME=!user_name!
 )
 if !container_exists!==false (
 	docker compose run --name !container_name! !image_name!
